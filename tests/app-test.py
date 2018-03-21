cd925aecd4bec753ed4ded70ab6372f333ca2ebd
phemmz <phemmzmcllroy@gmail.com>
2018-03-21 12:39:16 +0100

    testing ci build

--- tests/app-test.py
+++ tests/app-test.py
@@ -7,7 +7,7 @@ class MyTest(unittest.TestCase):
   def test_my_function(self):
     self.assertEqual(app.my_function(1, 1), 2)
     self.assertEqual(app.my_function(1, -1), 0)
-    self.assertEqual(app.my_function(0, 0), 0)
+    self.assertEqual(app.my_function(1, 0), 1)
     self.assertEqual(app.my_function(-1, -1), -2)
     self.assertEqual(app.my_function(1.0, 1), 2)
     self.assertEqual(app.my_function(1.1, 1.1), 2.2)
