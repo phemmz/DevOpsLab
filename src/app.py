75c9df01003b30f7313c852efbc39376fd8a3565
phemmz <phemmzmcllroy@gmail.com>
2018-03-21 12:32:50 +0100

    First commit

---
+++ src/app.py
@@ -0,0 +1,3 @@
+def my_function(param1, param2):
+  return param1 + param2
+
---
+++ tests/app-test.py
@@ -0,0 +1,16 @@
+import os
+os.sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import unittest
+from src import app
+
+class MyTest(unittest.TestCase):
+  def test_my_function(self):
+    self.assertEqual(app.my_function(1, 1), 2)
+    self.assertEqual(app.my_function(1, -1), 0)
+    self.assertEqual(app.my_function(0, 0), 0)
+    self.assertEqual(app.my_function(-1, -1), -2)
+    self.assertEqual(app.my_function(1.0, 1), 2)
+    self.assertEqual(app.my_function(1.1, 1.1), 2.2)
+
+if __name__ == '__main__':
+  unittest.main()
